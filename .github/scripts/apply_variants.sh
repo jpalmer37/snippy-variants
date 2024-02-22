52b6c1e833b7240c0b432d600363cac0a116e972
Dan Fornika <dfornika@gmail.com>
2024-02-21 16:39:21 -0800

    Add testing pipeline

---
+++ .github/scripts/apply_variants.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+.github/scripts/apply_variants.py --genome .github/data/assemblies/NC_000962.3.fa --variants .github/data/variants/DRR261178.vcf -o .github/data/assemblies/DRR261178.fa
+.github/scripts/apply_variants.py --genome .github/data/assemblies/NC_000962.3.fa --variants .github/data/variants/DRR261179.vcf -o .github/data/assemblies/DRR261179.fa
+.github/scripts/apply_variants.py --genome .github/data/assemblies/NC_000962.3.fa --variants .github/data/variants/DRR261180.vcf -o .github/data/assemblies/DRR261180.fa
+.github/scripts/apply_variants.py --genome .github/data/assemblies/NC_000962.3.fa --variants .github/data/variants/DRR261181.vcf -o .github/data/assemblies/DRR261181.fa
